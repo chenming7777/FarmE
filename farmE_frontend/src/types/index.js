a7d80eb908f04e534a9e1b641d06588fe8c89b13
rand0mguylol <72346097+rand0mguylol@users.noreply.github.com>
2024-09-16 22:06:29 +0800

    icons & graph

--- farmE_frontend/src/types/index.js
+++ farmE_frontend/src/types/index.js
@@ -5,8 +5,8 @@ export const sidebarList = {
     url: "/dashboard",
   },
   "Dashboard Farmer": {
-    active: "src/assets/sidebar/active/home.png",
-    inactive: "src/assets/sidebar/inactive/home.png",
+    active: "src/assets/sidebar/active/farmer.png",
+    inactive: "src/assets/sidebar/inactive/farmer.png",
     url: "/dashboard-farmer",
   },
   "Virtual Twin": {
@@ -25,13 +25,13 @@ export const sidebarList = {
     url: "/system-care",
   },
   "Solar Insights": {
-    active: "src/assets/sidebar/active/tool.png",
-    inactive: "src/assets/sidebar/inactive/tool.png",
+    active: "src/assets/sidebar/active/insight.png",
+    inactive: "src/assets/sidebar/inactive/insight.png",
     url: "/solar-insights",
   },
   "Smart Assistant": {
-    active: "src/assets/sidebar/active/tool.png",
-    inactive: "src/assets/sidebar/inactive/tool.png",
+    active: "src/assets/sidebar/active/assistant.png",
+    inactive: "src/assets/sidebar/inactive/assistant.png",
     url: "/smart-assistant",
   },
 };
@@ -43,13 +43,13 @@ export const sidebarDown = {
     url: "/notification",
   },
   "Solar Companies": {
-    active: "src/assets/sidebar/active/tool.png",
-    inactive: "src/assets/sidebar/inactive/tool.png",
+    active: "src/assets/sidebar/active/companies.png",
+    inactive: "src/assets/sidebar/inactive/companies.png",
     url: "/solar-companies",
   },
   Subscription: {
-    active: "src/assets/sidebar/active/tool.png",
-    inactive: "src/assets/sidebar/inactive/tool.png",
+    active: "src/assets/sidebar/active/sub.png",
+    inactive: "src/assets/sidebar/inactive/sub.png",
     url: "/subscription",
   },
 };
